6b4a6f14ac54586e155bb24eabd479a1cc79df7d
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-11 21:38:25 -0500

    Update with code from lessons 8 and 9 (Dec 6 & 8)

--- src/Stats.java
+++ src/Stats.java
@@ -1,23 +1,35 @@
-// Started in Lesson 7 (Dec 1)
+// Used in lessons 7, 8 and 9
 public class Stats {
     public static void main(String[] args) {
+        double foo = 0.5;
+        double bar = f(foo);
+        foo = bar;
+        foo = 5;
         int[] values = new int[5];
         values[0] = 7;
         values[1] = 3;
         values[2] = 4;
         values[3] = -2;
         values[4] = 12;
-        printArray(values);
-
         int smallest = minimum(values);
+
+        System.out.println(f(13));
     }
 
     public static int minimum(int[] dataset) {
-        // ... we'll get to this ...
+        int minSoFar = dataset[0];
+        for (int i = 1; i < dataset.length; i++) {
+            int element = dataset[i];
+            if (element < minSoFar) {
+                minSoFar = element;
+            }
+        }
+        return minSoFar;
     }
 
-    public static double triple(double input) {
-        return input * 3;
+    // An example method, like the algebra function 'f(x) = 2x'
+    public static double f(double x) {
+        return 2 * x;
     }
 
     public static void printArray(int[] fred) {
